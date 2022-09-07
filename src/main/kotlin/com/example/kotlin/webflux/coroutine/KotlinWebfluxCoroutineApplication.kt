4ea60055c8ddaa0cb3250594be993704c220bb60
ljw0829 <ljw0829@midasin.com>
2022-09-07 16:21:39 +0900

    - feat : kotlin-webflux-coroutine 초기 세팅

---
+++ src/main/kotlin/com/example/kotlin/webflux/coroutine/KotlinWebfluxCoroutineApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kotlin.webflux.coroutine
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinWebfluxCoroutineApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinWebfluxCoroutineApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/kotlin/webflux/coroutine/KotlinWebfluxCoroutineApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotlin.webflux.coroutine
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinWebfluxCoroutineApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
